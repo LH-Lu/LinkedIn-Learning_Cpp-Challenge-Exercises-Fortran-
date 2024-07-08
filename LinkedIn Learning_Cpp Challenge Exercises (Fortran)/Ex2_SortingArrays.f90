27dbdd9748ac3feadbe21d601f3a99cd92632bf0
L Lv <luleheng@live.com>
2024-07-08 16:47:05 +0800

    Updated Ex2 to use same sorting algo as the one used in the Cpp version

--- LinkedIn Learning_Cpp Challenge Exercises (Fortran)/Ex2_SortingArrays.f90
+++ LinkedIn Learning_Cpp Challenge Exercises (Fortran)/Ex2_SortingArrays.f90
@@ -1,5 +1,5 @@
 ! Ex2: Sorting Arrays
-! As an exercise, implement a simple sorting algorithm (not the one done in C++ version) to sort an int array in ascending order only
+! As an exercise, implement a sorting algorithm to sort an int array.
     
 ! Note to self, before and after running script, to prevent MS VS linking errors during compilation due to multiple files present: 
 ! Solution Explorer --> r/click on file name --> properties --> Toggle 'Exclude from build'
@@ -9,32 +9,73 @@
         implicit none
         
     CONTAINS
-        subroutine ArraySort (arr, ArrSize)
+        subroutine ArraySort (arr, ArrSize, IsDescend)
         
             implicit none
         
             ! Declare arguments
             integer, intent (inout) :: arr(:)
             integer, intent (in) :: ArrSize
+            logical, intent (in) :: IsDescend
             
             ! Initialise fn variables
-            integer :: idx, idx2, MinVal, MinIdx
+            integer :: idx1, idx2, MinValue, MaxValue, MinValueIdx, MaxValueIdx
+            integer :: LeftMostEntryIdx, RightMostEntryIdx, LeftMostEntry, RightMostEntry
+            integer :: tempDescend
             
-            ! Sort array (selection sort algo)
-            do idx = 1, ArrSize
-                MinVal = arr(idx)
-                MinIdx = idx
+            ! Sort array (modified selection sort algo)
+            do idx1 = 1, ArrSize/2
+               ! Initialise for new pass
+                MinValue = arr(idx1)
+                MaxValue = arr(idx1)
+                MinValueIdx = idx1
+                MaxValueIdx = idx1
                 
-                do idx2 = idx, ArrSize
-                    if (arr(idx2) < MinVal) then
-                        MinVal = arr(idx2)
-                        MinIdx = idx2
+                RightMostEntry = arr(ArrSize - idx1 + 1)
+                RightMostEntryIdx = ArrSize - idx1 + 1
+                
+                ! Finding Min and Max values
+                do idx2 = idx1, ArrSize - idx1 + 1
+                    if (arr(idx2) < MinValue) then
+                        MinValue = arr(idx2)
+                        MinValueIdx = idx2
                     end if
                     
+                    if (arr(idx2) > MaxValue) then
+                        MaxValue = arr(idx2)
+                        MaxValueIdx = idx2
+                    end if
                 end do
                 
-                arr(MinIdx) = arr(idx)
-                arr(idx) = MinVal
+                ! Swap Min Value and Left Most Entry value
+                LeftMostEntryIdx = idx1
+                LeftMostEntry = arr(LeftMostEntryIdx)
+                arr(LeftMostEntryIdx) = MinValue
+                arr(MinValueIdx) = LeftMostEntry
+                
+                if (MaxValueIdx == LeftMostEntryIdx) then ! Need to update MaxValueIdx if it moves
+                    MaxValueIdx = MinValueIdx
+                end if
+                
+                ! Swap Max Value and Right Most Entry value
+                RightMostEntryIdx = ArrSize - idx1 + 1
+                RightMostEntry = arr(RightMostEntryIdx)
+                arr(RightMostEntryIdx) = MaxValue
+                arr(MaxValueIdx) = RightMostEntry
+                
+            end do
+            
+            if (IsDescend == .false.) then
+                return
+            end if
+            
+            ! Rearrange array in descending order
+            do idx1 = 1, ArrSize/2
+                RightMostEntryIdx = ArrSize - idx1 + 1
+                
+                tempDescend = arr(idx1)
+                arr(idx1) = arr(RightMostEntryIdx)
+                arr(RightMostEntryIdx) = tempDescend
             end do
             
         end subroutine ArraySort
@@ -53,6 +94,8 @@
         integer, parameter :: MAX_ARRAY = 100
         integer, dimension(MAX_ARRAY) :: arr
         integer :: ArrSize
+        logical :: IsDescend
+        integer :: IsDescendInt
         
         integer :: idx1
         
@@ -67,15 +110,26 @@
             read (*, '(I)') arr(idx1)
         end do
         
+        print '(A, \)', "Sort the array in descending order? 0 - NO, 1 - YES: "
+        read (*, '(I)') IsDescendInt
+        
+        select case (IsDescendInt)
+        case (0)
+            IsDescend = .false.
+        case default
+            IsDescend = .true.
+        end select
+        
         print '(A, \)', "The array entered is: "
         call print_arrays()
         
         ! Sort arrays
-        call ArraySort(arr, ArrSize)
+        call ArraySort(arr, ArrSize, IsDescend)
         
         ! Print result
         print '(A, \)', "The sorted array is: "
         call print_arrays()
+        
         
         read(*, '(A)') endchar
         

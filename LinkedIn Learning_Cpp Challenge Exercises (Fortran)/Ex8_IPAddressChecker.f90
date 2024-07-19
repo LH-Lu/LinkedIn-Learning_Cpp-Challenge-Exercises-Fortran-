18b0776b3edf3961df00f92325a05c676c322758
L Lv <luleheng@live.com>
2024-07-19 17:54:20 +0800

    Completed Exercise 8

--- LinkedIn Learning_Cpp Challenge Exercises (Fortran)/Ex8_IPAddressChecker.f90
+++ LinkedIn Learning_Cpp Challenge Exercises (Fortran)/Ex8_IPAddressChecker.f90
@@ -14,14 +14,92 @@ PROGRAM Ex8_IPChecker
     
     implicit none
     
-    real :: a
+    ! Declare variables
+    integer, parameter :: MAX_IP_LENGTH = 100
+    character (len = MAX_IP_LENGTH) :: IPaddress
+    logical :: IsValidIP = .false.
+    
     character :: endchar
     
-    read (*, *) a
-    print '(F)', a
+    ! Get IP Address from user
+    print '(A, \)', "Enter IP Address in IPv4 format: "
+    read (*, '(A)') IPaddress
+    
+    ! Check if IP Address is valid
+    IsValidIP = CheckIP(trim(IPaddress))
+    
+    ! Return result
+    print '(A, \)', "IP Address Validity Status: "
+    if (IsValidIP == .true.) then
+        print '(A)', "VALID"
+    else
+        print '(A)', "INVALID"
+    end if
+    
     
     read (*, '(A)') endchar
 
-
-
+    
+CONTAINS
+    FUNCTION CheckIP(IP)
+        
+        implicit none
+        
+        ! Declare argument
+        character (len = *), intent(in) :: IP
+        
+        ! Declare return
+        logical :: CheckIP
+        
+        ! Declare variables
+        integer :: idx, IPlength, TokenVal, NumOfPeriods, DigitStartIdx, DigitEndIdx
+        integer, parameter :: MinTokenVal = 0
+        integer, parameter :: MaxTokenVal = 255
+        integer, parameter :: ReqNumOfPeriods = 3
+        character (len = *), parameter :: ValidChars = '0123456789.'
+        
+        ! Get length of IPAddress string
+        IPlength = len(IP)
+        
+        ! Check if IP Address entered is only digits and periods 
+        if (verify(IP, ValidChars) /= 0) then
+            CheckIP = .false.
+            return
+        end if
+        
+        ! Loop through IP Address string. Check: 1) token value 2) number of token digits entered (i.e must have 4 numbers or 3 periods)
+        NumOfPeriods = 0
+        DigitStartIdx = 1
+        DigitEndIdx = 1
+        
+        do idx = 1, IPlength
+            if (IP(idx:idx) == '.' .or. idx == IPLength) then
+                if (IP(idx:idx) == '.') then
+                    NumOfPeriods = NumOfPeriods + 1
+                    DigitEndIdx = idx - 1
+                end if
+                if (idx == IPLength) then
+                    DigitEndIdx = idx
+                end if
+                
+                read (IP(DigitStartIdx:DigitEndIdx), '(I)') TokenVal
+                
+                if (TokenVal < MinTokenVal .or. TokenVal > MaxTokenVal) then
+                    CheckIP = .false.
+                    return
+                end if
+                
+                ! re-initialise for next round
+                DigitStartIdx = idx + 1
+            end if
+        end do
+        
+        if (NumOfPeriods /= ReqNumOfPeriods) then
+            CheckIP = .false.
+            return
+        end if
+        
+        CheckIP = .true.
+        
+    END FUNCTION CheckIP
 END PROGRAM Ex8_IPChecker

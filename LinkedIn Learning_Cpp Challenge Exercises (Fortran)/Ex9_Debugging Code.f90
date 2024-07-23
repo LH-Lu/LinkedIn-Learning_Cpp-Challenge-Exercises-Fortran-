94a071cd101294da18470a007eca2155e3f9e8b7
L Lv <luleheng@live.com>
2024-07-23 14:45:12 +0800

    Completed Exercise 9 (please read comments)

---
+++ LinkedIn Learning_Cpp Challenge Exercises (Fortran)/Ex9_Debugging Code.f90
@@ -0,0 +1,5 @@
+! Ex9: Debugging code
+! Note, this is not completed or attempted as the original exercise focused on debugging an exisitng C++ code.
+
+! Note to self, before and after running script, to prevent MS VS linking errors during compilation due to multiple files present: 
+! Solution Explorer --> r/click on file name --> properties --> Toggle 'Exclude from build'
